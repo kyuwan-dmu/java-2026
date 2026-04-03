906b6f091d79be5f3bd2c26705dd8ec81698dbd0
kimhyomin~ <khm83029@gmail.com>
2026-04-03 23:48:41 +0900

    add : number print

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(" 1 2 3 4\n");
+        System.out.print(" 1 2 3 4\n");
+        System.out.print(" 1 2 3 4\n");
+        System.out.print(" 1 2 3 4");
+    }
+}
