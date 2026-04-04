54284407ecbbbdbb92250edf9fd9816d43720212
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-04-04 15:12:55 +0900

    수정 push

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int n;
+        int i;
+        for(i = 1; i < 5; i ++){
+            for (n = 1; n < 5; n ++){
+                System.out.printf("%d ",n);
+            }
+            System.out.println("");
+        }
+    }
+}
