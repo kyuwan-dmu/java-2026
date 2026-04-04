9f8b35a22b8776610e2f1597f9520cfb70e43763
HAJUN PARK <wjsrnth12@gmail.com>
2026-04-04 18:24:51 +0900

    week4
    4주차 수업

---
+++ src/week4/Student.java
@@ -0,0 +1,14 @@
+package week4;
+
+/**
+ * Created by [HAJUN PARK] on 2026-04-04.
+ */
+public class Student {
+    String name;
+    int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
---
+++ src/week4/week4.java
@@ -0,0 +1,36 @@
+package week4;
+/**
+ * Created by [HAJUN PARK] on 2026-04-04.
+ */
+public class week4 {
+    public static void main(String[] args) {
+        Student[] students = {
+            new Student("학생1", 100),
+            new Student("학생1", 100),
+            new Student("학생1", 100)
+        };
+            // 학생의 이름과 점수를 받고, 출력을 합니다.
+
+            // 학생 점수 입력
+            // 배열의 선언
+            // 1) 선언과 동시에 값을 한 번에 넣어버림 -> 넣은 값 만큼 공간이 알아서 생김.
+//            String[] students = {"학생1", "학생2", "학생3"};
+//
+//            // 2) 선언 후 값을 입력
+//            String[] students1 = new String[10];
+//            int[] scores1 = new int[10];
+//
+//            System.out.println(">>>>>>"+students1[0]);
+//            System.out.println(">>>>>>"+scores1[0]);
+            // 힉생점수 출력
+//        System.out.println(students[0] + " 의 점수 : " + scores[0]);
+//        System.out.println(students[1] + " 의 점수 : " + scores[1]);
+//        System.out.println(students[2] + " 의 점수 : " + scores[2]);
+//        int[] a = {3, 4, 5};
+//        int[] b = a;
+//        b[0] = 0;
+//        System.out.println(a[0]);
+//        System.out.println(b[0]);
+            // 출력 값 : 0 0
+        }
+    }
