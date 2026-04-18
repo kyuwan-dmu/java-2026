de356331d45834896eb132f52236a310cbfe9af8
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-04-18 18:17:10 +0900

    feat: Student

--- src/week06/Week06Main.java
+++ src/week06/Week06Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import week06.student.Student;
 import week06.report.ReportService;
 
-public class Week06Main {
+public class Week06Main{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -29,9 +29,79 @@ public class Week06Main {
             switch (menu) {
                 case 0:
                     System.out.println("프로그램을 종료합니다.");
-                    return;
-                default:
-                    System.out.println("TODO: 구현하세요");
+                    break;
+
+                case 1:
+                        System.out.print("이름 입력 (없으면 Enter): ");
+                        String name = sc.nextLine();
+                        if(name.isEmpty()) {
+                            name = "이름없음";
+                        }
+
+                        System.out.print("점수 입력 (없으면 Enter): ");
+                        String score = sc.next();
+
+                        if(score.isEmpty()) {
+                            score = "0";
+                        }
+
+                        if(Integer.parseInt(score) > 100 || Integer.parseInt(score) < 0) {
+                            System.out.println("0~100점 사이로 입력해주세요.");
+                            break;
+                        }
+
+                        students[count++] = new Student(name, Integer.parseInt(score));
+
+                        System.out.println("등록완료: " + name +"("+score+"점)");
+                    break;
+
+                case 2:
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("이름: " + students[i].getName()+", 점수: " + students[i].getScore()+", 등급: " + students[i].getRank());
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("이름 입력: ");
+                    name = sc.next();
+                    System.out.println("새 점수 입력: ");
+                    int num = sc.nextInt();
+
+                    if(num > 100 || num < 0) {
+                        System.out.println("유효하지 않은 점수입니다.");
+                        break;
+                    }
+
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].getName().equals(name)) {
+                            students[i].ResetScore(num);
+                        }
+                    }
+
+                    System.out.println("점수 수정 완료: " + name+" ("+num+"점)");
+
+                    break;
+                case 4:
+                    System.out.println("보너스 점수 입력:");
+                    num = sc.nextInt();
+
+                    for (int i = 0; i < count; i++) {
+                        students[i].AddScore(num);
+                    }
+
+                    System.out.println("보너스 부여 완료!");
+                    break;
+
+                case 5:
+                    int allScore = 0;
+                    for (int i = 0; i < count; i++) {
+                        allScore += students[i].getScore();
+                    }
+
+                    System.out.println("전체 평균: "+allScore/count);
+                    break;
+                case 6:
+                    System.out.println("총 학생 수: " + (count));
             }
         }
     }
--- src/week06/student/Student.java
+++ src/week06/student/Student.java
@@ -1,11 +1,60 @@
 package week06.student;
 
 public class Student {
+    enum RankType {
+        A,      //>= 90
+        B,      //>= 80
+        C,      //>= 70
+        F       //70 >
+    }
 
-    // TODO: 필드를 선언하세요
+    private String name;
+    private int score;
+    private RankType rank;
 
-    // TODO: 생성자를 만드세요
+    public String getName() {
+        return name;
+    }
 
-    // TODO: 메서드를 만드세요
+    public RankType getRank() {
+        return rank;
+    }
 
+    public int getScore() {
+        return score;
+    }
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+
+        SelectRank();
+    }
+
+    private void SelectRank() {
+        if(score >= 90) {
+            rank = RankType.A;
+        } else if(score >= 80) {
+            rank = RankType.B;
+        } else if(score >= 70) {
+        rank = RankType.C;
+        } else {
+            rank = RankType.F;
+        }
+    }
+
+    public void ResetScore(int score) {
+        this.score = score;
+        SelectRank();
+    }
+
+    public void AddScore(int score) {
+        this.score += score;
+
+        if (score > 100) {
+            score = 100;
+        }
+
+        SelectRank();
+    }
 }
