c7070473fb3a36bd28b7c53f89caad853e37db19
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-04-18 18:39:53 +0900

    작성중

--- src/week06/Week06Main.java
+++ src/week06/Week06Main.java
@@ -9,9 +9,13 @@ public class Week06Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Student[] students = new Student[100];
+        String name_tmp;
+        int score_tmp;
         int count = 0;
 
         while (true) {
+            name_tmp = "";
+            score_tmp = 0;
             System.out.println("\n===== 성적 관리 시스템 =====");
             System.out.println("1. 학생 등록");
             System.out.println("2. 전체 학생 목록 조회");
@@ -30,6 +34,32 @@ public class Week06Main {
                 case 0:
                     System.out.println("프로그램을 종료합니다.");
                     return;
+                case 1:
+                    System.out.print("이름 입력 (없으면 Enter): ");
+                    name_tmp = sc.nextLine().trim();
+                    System.out.print("점수 입력 (없으면 Enter): ");
+                    score_tmp = sc.nextInt();
+                    students[count] = new Student(name_tmp, score_tmp);
+                    System.out.println("등록 완료: " + students[count].getName() + " (" + students[count].getScore() + "점)");
+                    // System.out.println("test" + students[count].getName() + students[count].getScore());
+                    // text code
+                    ++count;
+                    break;
+                case 2:
+                    System.out.println("\n===== 전체 학생 목록 =====");
+                    for(int i = 0; i < count; i++) {
+                        System.out.println(students[i].getName() + " (" + students[i].getScore() + "점)");
+                    }
+                    System.out.println("========================");
+                    break;
+                case 3:
+                    boolean yesorno = true;
+                    while(yesorno) {
+                        System.out.print("이름 입력: ");
+                        name_tmp = sc.nextLine().trim();
+
+                    }
+                    System.out.print("이름 입력: ");
                 default:
                     System.out.println("TODO: 구현하세요");
             }
--- src/week06/student/Student.java
+++ src/week06/student/Student.java
@@ -2,6 +2,40 @@ package week06.student;
 
 public class Student {
 
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public char getGrade(int score) {
+        if (score>90)
+            return 'A';
+        else if (score>80)
+            return 'B';
+        else if (score>70)
+            return 'C';
+        else
+            return 'F';
+    }
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
     // TODO: 필드를 선언하세요
 
     // TODO: 생성자를 만드세요
@@ -9,3 +43,4 @@ public class Student {
     // TODO: 메서드를 만드세요
 
 }
+
