f4a5c18c7162dc1a1325a0bfa8cdc07f46d3f0c1
kwackgwon <chdltm787@gmail.com>
2026-04-23 15:22:22 +0900

    중간 저장

--- src/week06/Week06Main.java
+++ src/week06/Week06Main.java
@@ -13,7 +13,7 @@ public class Week06Main {
 
         while (true) {
             System.out.println("\n===== 성적 관리 시스템 =====");
-            System.out.println("1. 학생 등록");
+            System.out.println("1. 학생 등록"); //
             System.out.println("2. 전체 학생 목록 조회");
             System.out.println("3. 학생 점수 수정");
             System.out.println("4. 보너스 점수 일괄 부여");
@@ -29,11 +29,66 @@ public class Week06Main {
             switch (menu) {
                 case 0:
                     System.out.println("프로그램을 종료합니다.");
-                    return;
-                default:
-                    System.out.println("TODO: 구현하세요");
+                    break;
+                case 1:
+                    System.out.print("이름 입력 (없으면 Enter):");
+                    String name = sc.next();
+
+                    System.out.print("점수 입력 (없으면 Enter):");
+                    int score = sc.nextInt();
+                    if (score > 100 || score < 0) {
+                        System.out.print("유효하지 않는 점수입니다");
+                    }
+                    String grade = "F";
+                    if (score >= 90) {
+                        grade = "A";
+                    } else if (score >= 80) {
+                        grade = "B";
+                    } else if (score >= 70) {
+                        grade = "c";
+                    }
+                    students[count++] = new Student(name, score, grade);
+
+                    System.out.print("등록 완료: " + name + " " + score + " " +grade);
+                    break;
+                case 2:
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("이름: " + students[i].getName() + ", 점수: " + students[i].getScore());
+                    }
+                    break;
+                case 3:
+                    System.out.print("이름 입력 (없으면 Enter):");
+                    name = sc.next();
+                    System.out.print("새 점수 입력:");
+                    int nnum = sc.nextInt();
+                    if (nnum > 100 || nnum < 0) {
+                        System.out.print("유효하지 않는 점수입니다");
+                    }
+                    for (int i = 0; count > i; i++) {
+                        if (students[i].getName().equals(name)) {
+                            students[i].setScore(nnum);
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.print("보너스 점수 입력:");
+                    int b_score = sc.nextInt();
+                    for (int i = 0; i < count; i++) {
+                        students[i].setScore(students[i].getScore() + b_score);
+                    }
+                    System.out.print("보너스 부여 완료!");
+                    break;
+                case 5:
+                    int total = 0;
+                    for (int i = 0; i < count; i++) {
+                        total = students[i].getScore() + total;
+                    }
+                    System.out.print("전체 평균: " + (double) total / count);
+                    break;
+                case 6:
+                    System.out.print("총 학생 수: " + count);
+                    break;
             }
         }
     }
-
 }
--- src/week06/student/Student.java
+++ src/week06/student/Student.java
@@ -3,9 +3,48 @@ package week06.student;
 public class Student {
 
     // TODO: 필드를 선언하세요
+    String name;
+    int score;
+    String grade;
+
+    Student() {
+        String name = "이름없음";
+        int score = 0;
+        String grade = "F";
+    }
 
     // TODO: 생성자를 만드세요
+    public Student(String name, int score, String grade) {
+        this.name = name;
+        this.score = score;
+        this.grade = grade;
+    }
 
-    // TODO: 메서드를 만드세요
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student(int score) {
+        this.score = 0;
+    }
 
 }
+    // TODO: 메서드를 만드세요
+
+
