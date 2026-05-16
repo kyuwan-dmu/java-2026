9254deac6637b38ebb6661030a310dbf5e17b86e
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-05-16 17:27:37 +0900

    feat: 03 Main

--- src/practice03/Main.java
+++ src/practice03/Main.java
@@ -10,10 +10,33 @@ public class Main {
         Order[] orders = OrderDataProvider.getOrdersAsArray();
 
         // TODO 1: 브랜드가 "바디홀릭"인 주문만 출력하시오
+        int BHCount = 0; //브랜드 바디홀릭 카운트
+        int BHPrice = 0; //브랜드 바디홀릭 총 가격
+
+        System.out.println("===== 바디홀릭 주문 목록 =====");
+        for (int i = 0; i < orders.length; i++) {
+            if(orders[i].getBrand().equals("바디홀릭"))
+            {
+                BHCount++;
+                BHPrice += orders[i].getPrice();
+                System.out.println(orders[i]+" ("+ orders[i].getRegion()+")");
+            }
+        }
 
         // TODO 2: 바디홀릭 주문의 건수와 금액 합계를 출력하시오
+        System.out.println("===== 바디홀릭 주문 목록 =====");
+        System.out.println("바디홀릭 주문: "+BHCount+"건");
+        System.out.println("합계: "+String.format("%,d원", BHPrice));
+
+
+
 
         // TODO 3: 상품금액이 15,000원 이상인 주문만 출력하시오
-
+        for (int i = 0; i < orders.length; i++) {
+            if(orders[i].getBrand().equals("바디홀릭") && orders[i].getPrice() >= 15000)
+            {
+                System.out.println(orders[i]+" ("+ orders[i].getRegion()+")");
+            }
+        }
     }
 }
