e91e629c55318cd447c3341d76f0d7272745f3ed
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-05-16 18:46:44 +0900

    5/16

--- src/practice02/Main.java
+++ src/practice02/Main.java
@@ -12,7 +12,9 @@ public class Main {
         // TODO 1: 기본 for문으로 전체 주문을 번호와 함께 출력하시오
 
         // TODO 2: 향상된 for문(for-each)으로 전체 주문을 출력하시오
-
+        for (Order order : orders) {
+            System.out.println("[" + order.getBrand()  + "]");
+        }
         // TODO 3: 전체 주문 건수를 출력하시오
 
     }
