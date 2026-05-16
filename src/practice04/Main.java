1bec3ae45f05e645b4840de6d72e39be1492b410
shlee <shlee@data-flow.co.kr>
2026-05-16 17:20:28 +0900

    [feat] practice04 작성완료

--- src/practice04/Main.java
+++ src/practice04/Main.java
@@ -1,9 +1,10 @@
 package practice04;
 
 import data.OrderDataProvider;
-import model.Order;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import model.Order;
 
 public class Main {
 
@@ -12,12 +13,43 @@ public class Main {
         List<Order> orders = OrderDataProvider.getOrdersAsList();
 
         // TODO 1: 현재 주문 건수 출력
+        System.out.println("현재 주문 건수: " + orders.size() + "건\n");
 
         // TODO 2: 신규 주문 2건 추가 후 건수 출력
+        long maxOrderId = 0;
+        for (Order order : orders) {
+            long id = Long.parseLong(order.getOrderId());
+            if (id > maxOrderId) {
+                maxOrderId = id;
+            }
+        }
+
+        String newOrderId1 = String.valueOf(maxOrderId + 1);
+        String newOrderId2 = String.valueOf(maxOrderId + 2);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String orderDate = sdf.format(new Date());
+
+        Order newOrder1 = new Order(
+                newOrderId1, "테스트1", "", 1, "머지", 15000,
+                orderDate, "301 배송 준비 중", "서울 강남구");
+        Order newOrder2 = new Order(
+                newOrderId2, "테스트2", "", 1, "머지", 15000,
+                orderDate, "301 배송 준비 중", "서울 강남구");
+        orders.add(newOrder1);
+        orders.add(newOrder2);
+        System.out.println("[신규 주문 추가]");
+        System.out.println("추가 후 건수: " + orders.size() + "건\n");
 
         // TODO 3: 첫 번째 주문 삭제 후 건수 출력
+        orders.remove(0);
+        System.out.println("[첫 번째 주문 삭제]");
+        System.out.println("삭제 후 건수: " + orders.size() + "건\n");
 
         // TODO 4: 전체 주문 리스트 출력
-
+        System.out.println("===== 전체 주문 =====");
+        for (Order order : orders) {
+            System.out.println(order);
+        }
+        System.out.println("=====================");
     }
 }
