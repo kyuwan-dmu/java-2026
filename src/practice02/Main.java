1a3a011bf2b9962fde5fa869a1c910842a217b38
박준혁 <pkjun@gmail.com>
2026-05-16 17:54:10 +0900

    첫 번째, 두 번째, 세 번째 문제 풀이 완료

--- src/practice02/Main.java
+++ src/practice02/Main.java
@@ -15,5 +15,26 @@ public class Main {
 
         // TODO 3: 전체 주문 건수를 출력하시오
 
+        System.out.println("===== 전체 주문 목록 =====");
+
+        int count = 0;
+        for(int i=0; i<orders.length; i++) {
+            System.out.println(i+1 + ". " + orders[i].getProductName() + " - " + String.format("%,d", orders[i].getPrice()) + "원");
+            count++;
+        }
+        System.out.println("========================");
+        System.out.println("총 " + count + "건\n");
+
+
+        System.out.println("===== 전체 주문 목록 =====");
+
+        count = 0;
+        for(Order order : orders) {
+            System.out.println(count+1 + ". " + order.getProductName() + " - " + String.format("%,d", order.getPrice()) + "원");
+            count++;
+        }
+
+        System.out.println("========================");
+        System.out.println("총 " + count + "건");
     }
 }
--- src/practice03/Main.java
+++ src/practice03/Main.java
@@ -14,6 +14,34 @@ public class Main {
         // TODO 2: 바디홀릭 주문의 건수와 금액 합계를 출력하시오
 
         // TODO 3: 상품금액이 15,000원 이상인 주문만 출력하시오
+        System.out.println("===== 바디홀릭 주문 목록 =====");
+
+        int count = 0, sum = 0;
+        for(Order order : orders) {
+            if(order.getBrand().equals("바디홀릭")) {
+                System.out.println(order.toString());
+                sum += order.getPrice();
+                count++;
+            }
+        }
+        System.out.println("============================");
+        System.out.println("바디홀릭 주문: " + count + "건");
+        System.out.println("합계: " + String.format("%,d\n", sum));
+
+        System.out.println("===== 15,000원 이상 주문 =====");
+        count = 0;
+        sum = 0;
+        for(Order order : orders) {
+            if(order.getBrand().equals("바디홀릭")) {
+                if(15000 <= order.getPrice()) {
+                    System.out.println(order.toString());
+                    sum += order.getPrice();
+                    count++;
+                }
+            }
+        }
+        System.out.println("바디홀릭 주문: " + count + "건");
+        System.out.println("합계: " + String.format("%,d\n", sum));
 
     }
 }
