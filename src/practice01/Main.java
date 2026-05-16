642da34f3975d8440cdca4ac8ec843191036c7ca
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-05-16 16:47:34 +0900

    중간저장

--- src/practice01/Main.java
+++ src/practice01/Main.java
@@ -10,6 +10,23 @@ public class Main {
         Order[] orders = OrderDataProvider.getOrdersAsArray();
 
         // TODO: 첫 번째 주문을 변수에 담고, 모든 정보를 출력하시오
+        String First = String.valueOf(orders[0]);
+
+        System.out.println("===== 주문 상세 =====");
+        System.out.println("주문번호: "+orders[0].getOrderId());
+        System.out.println("상품명: "+orders[0].getProductName());
+        System.out.println("브랜드: "+orders[0].getOption());
+        System.out.println("옵션: "+orders[0].getQuantity());
+        System.out.println("수량: "+orders[0].getBrand());
+        System.out.println("금액: "+orders[0].getPrice());
+        System.out.println("주문일: "+orders[0].getOrderDate());
+        System.out.println("상태: "+orders[0].getStatus());
+        System.out.println("상태: "+orders[0].getRegion());
+        System.out.println("====================");
+
+
+
+
 
     }
 }
