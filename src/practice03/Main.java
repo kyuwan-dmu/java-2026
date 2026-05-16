6c987cb66cd4aff2dbf31c17fa4602d8d6a533af
DESKTOP-KUIA7C1\109-1 <dongyang@dongyang.ac.kr>
2026-05-16 18:28:05 +0900

    feat : ã3번 무식한 방법으로 끝...

--- src/practice03/Main.java
+++ src/practice03/Main.java
@@ -8,12 +8,31 @@ public class Main {
     public static void main(String[] args) {
         // 데이터 불러오기
         Order[] orders = OrderDataProvider.getOrdersAsArray();
-
+        Order[] selectedOrders = new Order[orders.length];
         // TODO 1: 브랜드가 "바디홀릭"인 주문만 출력하시오
+        int index = 0;
+        int totalPrice = 0;
+        int orderCount = 0;
+        for(Order order:orders){
+            if("바디홀릭".equals(order.getBrand()) && order.getPrice() >= 15000 ){
+                selectedOrders[index] = order;
+                index += 1;
+                totalPrice += order.getPrice();
+                orderCount += 1;
+                System.out.println(order);
+            }
+        }
+        System.out.println("===== 바디홀릭 주문 목록 =====");
+        for(Order selectedorder: selectedOrders){
+            if(selectedorder != null){
+                System.out.println(selectedorder);
+            }
 
-        // TODO 2: 바디홀릭 주문의 건수와 금액 합계를 출력하시오
+        }
+        System.out.println("============================");
+        System.out.println("바디홀릭 주문 : " + orderCount + "건");
+        System.out.println("합계 : " + totalPrice + "원");
 
-        // TODO 3: 상품금액이 15,000원 이상인 주문만 출력하시오
 
     }
 }
